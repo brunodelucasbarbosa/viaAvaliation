fde3470569f0ded49d38a8a3c5946e199d0ad785
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2023-04-06 19:12:20 -0300

    first commit: start project

---
+++ src/main/java/br/com/via/avaliation/viaavaliation/ViaAvaliationApplication.java
@@ -0,0 +1,13 @@
+package br.com.via.avaliation.viaavaliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaAvaliationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaAvaliationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/via/avaliation/viaavaliation/ViaAvaliationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.via.avaliation.viaavaliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaAvaliationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
