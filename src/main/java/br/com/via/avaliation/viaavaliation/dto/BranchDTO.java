0b09109827191c744a04e7e07b4f397880710b03
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2023-04-11 11:12:25 -0300

    circuit breaker simple

--- src/main/java/br/com/via/avaliation/viaavaliation/dto/BranchDTO.java
+++ src/main/java/br/com/via/avaliation/viaavaliation/dto/BranchDTO.java
@@ -1,7 +1,5 @@
 package br.com.via.avaliation.viaavaliation.dto;
 
-import java.time.LocalDateTime;
-
 public record BranchDTO(Long id,
                         String name,
                         String cnpj,
@@ -10,4 +8,5 @@ public record BranchDTO(Long id,
                         String type,
                         Boolean active,
                         String created_at,
-                        String updated_at) {}
+                        String updated_at) {
+}
--- src/main/java/br/com/via/avaliation/viaavaliation/integration/BranchService.java
+++ src/main/java/br/com/via/avaliation/viaavaliation/integration/BranchService.java
@@ -2,36 +2,41 @@ package br.com.via.avaliation.viaavaliation.integration;
 
 import br.com.via.avaliation.viaavaliation.dto.BranchDTO;
 import br.com.via.avaliation.viaavaliation.exception.ResourceNotFoundException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class BranchService implements InterfaceBranchService {
 
+    private static final String URL_BRANCHES = "http://localhost:1080/api/v1/branches";
+
     RestTemplate restTemplate = new RestTemplate();
+
     @Override
+    @CircuitBreaker(name = "branchServiceCB")
     public List<BranchDTO> getAllBranches() {
         return Arrays.stream(
                 Objects.requireNonNull(
                         restTemplate.getForObject(
-                                "http://localhost:1080/api/v1/branches", BranchDTO[].class))).toList();
+                                URL_BRANCHES, BranchDTO[].class))).toList();
     }
 
     @Override
+    @CircuitBreaker(name = "branchServiceCB")
     public BranchDTO getBranchById(Long id) {
-        return Arrays.stream(Objects.requireNonNull(restTemplate.getForObject("http://localhost:1080/api/v1/branches", BranchDTO[].class)))
+        return Arrays.stream(Objects.requireNonNull(restTemplate.getForObject(URL_BRANCHES, BranchDTO[].class)))
                 .filter(element -> Objects.equals(element.id(), id))
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("Branch not found"));
     }
 
     @Override
+    @CircuitBreaker(name = "branchServiceCB")
     public void validateIfBranchExists(Long id) {
-        var exists = Arrays.stream(Objects.requireNonNull(restTemplate.getForObject("http://localhost:1080/api/v1/branches", BranchDTO[].class))).filter(
+        var exists = Arrays.stream(Objects.requireNonNull(restTemplate.getForObject(URL_BRANCHES, BranchDTO[].class))).filter(
                 element -> Objects.equals(element.id(), id)
         ).findFirst();
         if (exists.isEmpty()) {
